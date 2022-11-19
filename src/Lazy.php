b6004393fd2c81d4009f883868436ec57b8ef984
jeff-cannapress <jeff@cannapress.tech>
2022-11-19 15:35:58 +0000

    lazy

---
+++ src/Lazy.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CannaPress\Util;
+
+class Lazy
+{
+    private $have_constructed = false;
+    private $inner_value = null;
+    public function __construct(private callable $factory)
+    {
+    }
+    public function __get($name)
+    {
+        if ('value' === $name) {
+            if (!$this->have_constructed) {
+                $this->inner_value = ($this->factory)();
+                $this->have_constructed = true;
+            }
+            return $this->inner_value;
+        }
+    }
+}
